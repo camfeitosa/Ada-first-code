b9f7e836ea34fd14c9da129a84ef574686b351e4
Camila Feitosa <114369214+camfeitosa@users.noreply.github.com>
2023-06-25 20:05:18 -0300

    edit

---
+++ mod5/teste.js
@@ -0,0 +1,25 @@
+
+// var w = ''
+// var x;
+// var y = undefined
+// var z = 0 
+
+// console.log(w, x, y, z)
+// console.log(typeof(w))
+
+// // string undefined undefined 0
+
+// var x = 10
+// console.log(x+y)
+// let y = 15 error
+
+// if (50 > 100){
+//     console.log("Falta um tanto")
+// } else {
+//     if ("1" == "-1"){ 
+//         console.log('Quase lá')
+//     } if (!null){
+//         console.log("sucesso")
+//     }
+// }
+
