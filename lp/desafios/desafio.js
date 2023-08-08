46c1f3cc640e8ac738f2f516f4ee48342c5f51e2
Camila Feitosa <114369214+camfeitosa@users.noreply.github.com>
2023-08-07 22:56:38 -0300

    aula5

--- lp/desafios/desafio.js
+++ lp/desafios/desafio.js
@@ -1,63 +1,47 @@
-
-// // Em um sistema de vendas é emitido um recibo a cada venda, esse recibo está em formato string com a seguinte estrutura: 
-// // nome-do-produto1/valor33.5=cupom5;nome-do-produto2/valor4.99=cupom0;nome-do-produto3/valor10=cupom0;...
-// // As vendas são divididas por ; (ponto e virgula)
-
-// // Sendo: 
-// // nome-do-produto1 até a / (barra)     o nome do produto
-// // valor33.5 o valor do produto até o = (igual)
-// // cupom5 a porcentagem de desconto do produto após o = (igual)
-
-
-// // Precisamos formatar essa string para obter uma lista de objetos com a seguinte estrutura: 
-
-// const listaDaVenda = [
-//  b9
-// ]
-
-// // Extra: 
-// // Totalização da venda
-// // Além da lista completa formatada da venda, seria interessante obter a totalização dessa venda para apresentar ao cliente final
-// // Dado essa importância os seguintes dados devem ser apresentado em um objeto: 
-
-// const totais = {
-//     valorTotal: 999, // Valor total da venda
-//     valorTotalDesconto: 999, // Valor total com desconto
-//     quantidadeDeProdutos: 999, // Quantidade de produtos na venda
-// }
-
-// const reciboDeVenda = 'régua/valor3=cupom0;lápis/valor0.5=cupom0;mochila/valor50=cupom10;estojo/valor8=cupom0;cola/valor4=cupom0;cola/valor4=cupom0;mochila/valor50=cupom10;lápis/valor0.5=cupom0;cola/valor4=cupom0;lápis/valor0.5=cupom0;mochila/valor50=cupom10;tesoura/valor5=cupom0;caneta/valor1=cupom0;cola/valor4=cupom0;estojo/valor8=cupom0;borracha/valor2=cupom0;caderno/valor15=cupom5;lápis/valor0.5=cupom0;lápis/valor0.5=cupom0;tesoura/valor5=cupom0;'
-
-// // Boa sorte =D
+// Corrigido
 
 const reciboDeVenda = 'régua/valor3=cupom0;lápis/valor0.5=cupom0;mochila/valor50=cupom10;estojo/valor8=cupom0;cola/valor4=cupom0;cola/valor4=cupom0;mochila/valor50=cupom10;lápis/valor0.5=cupom0;cola/valor4=cupom0;lápis/valor0.5=cupom0;mochila/valor50=cupom10;tesoura/valor5=cupom0;caneta/valor1=cupom0;cola/valor4=cupom0;estojo/valor8=cupom0;borracha/valor2=cupom0;caderno/valor15=cupom5;lápis/valor0.5=cupom0;lápis/valor0.5=cupom0;tesoura/valor5=cupom0;'
 
-const venda = reciboDeVenda.split(';').filter(Boolean)
+const produtos = reciboDeVenda.split(';').filter(Boolean)
 
-const produtos = venda.reduce((lista, produto) => {
-  const [nomeProduto, valorCupom] = produto.split('/')
-  const [valorProduto, cupom] = valorCupom.split('=').map(item => Number(item.slice(5)))
+const produtosSemRepeticao = [...new Set(produtos.map(produto => produto))]
 
-  if (!lista[nomeProduto]) {
-    lista[nomeProduto] = {
-      produto: nomeProduto[0].toUpperCase() + nomeProduto.slice(1),
-      valor: valorProduto,
-      cupom,
-      quantidade: 0,
-    };
-  }
+const compra = produtosSemRepeticao.map(produto => {
+    const [nome, valores] = produto.split('/')
+    const [textoValor, textoCupom] = valores.split('=')
 
-  lista[nomeProduto].quantidade++
+    const valor = +textoValor.replace('valor', '')
+    const cupom = +textoCupom.replace('cupom', '')
+    const quantidade = produtos.filter(item => item === produto).length
 
-  return lista
-}, {})
+    return {
+      produto: nome.slice(0,1).toUpperCase() + nome.slice(1),           valor, 
+        cupom, 
+        quantidade
+    }
+})
 
-const listaDaVenda = Object.values(produtos)
+console.log(compra)
 
-const totalComDesconto = listaDaVenda.reduce((total, produto) => {
-  return total += (produto.valor * produto.quantidade) - (produto.valor * ((produto.cupom / 100) * produto.quantidade))
-}, 0)
 
-console.log(listaDaVenda)
+// const produtos = reciboDeVenda.split(';').filter(Boolean)
 
-console.log(totalComDesconto)
+// const compra = produtos.map(produto =>{
+//   const [nome, valores] = produto.split('/')
+//   const [textoValor, textoCupom] = valores.split('=')
+
+//   const valor = parseFloat(textoValor.replace('valor', '')) // + ou parse 
+//   const cupom = +textoCupom.replace('cupom', '')
+//   const quant = produtos.filter(item => item === produto).length
+
+//   // console.log({nome, valores, cupom, quant})
+
+//   return{
+//     produto: nome,
+//     valor,
+//     cupom,
+//     quant
+//   }
+// })
+
+// console.log(compra)
---
+++ lp/projetoFinal/script.js
@@ -0,0 +1,18 @@
+// escola Lógica
+
+const turma = {
+    codigo: 10, 
+    maximo: 7
+}
+
+const aluno = {
+    nome: 'Camila',
+    sobrenome: 'Feitosa',
+    email: 'aaa@gmail.com',
+    turma: 10,
+    nascimento: 7/7/2006,
+    notas: [1, 2, 3, 4],
+    ativo: true
+}
+
+console.log(aluno, turma)
