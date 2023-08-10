b14d3cda555da5abb786c38b7e82ae791c464694
Camila Feitosa <114369214+camfeitosa@users.noreply.github.com>
2023-08-10 09:37:24 -0300

    desafio

--- lp/projetoFinal/script.js
+++ lp/projetoFinal/script.js
@@ -1,18 +1,81 @@
-// escola Lógica
+// Cadastrar turma contendo
+
+// código, number entre 1 e 10 (no máximo 10 turmas)
+// máximo, number máximo de alunos de 5 a 10
+
+//Relacionamento
+//Entidades: Turma e aluno
+
+//Cadastrar turma
+// Turma com o código existente = duplicado
+
+
+// EXEMPLO DO PROF
 
 const turma = {
-    codigo: 10, 
-    maximo: 7
+    codigo: 0,
+    maximoDeAlunos: 0 
 }
 
-const aluno = {
-    nome: 'Camila',
-    sobrenome: 'Feitosa',
-    email: 'aaa@gmail.com',
-    turma: 10,
-    nascimento: 7/7/2006,
-    notas: [1, 2, 3, 4],
-    ativo: true
+const turmas = []
+
+// Cadastro de turmas
+// Turma com o código já existente
+const cadastrarTurma = ({codigo, maximoDeAlunos}) => {
+
+    // const turmaEncontrada = turmas.filter(turma => turma.codigo === codigo).length
+    const turmaEncontrada = turmas.find(turma => turma.codigo === codigo)
+
+    codigo = parseInt(codigo)
+    maximoDeAlunos = parseInt(maximoDeAlunos)
+
+    if(!codigo || !maximoDeAlunos){
+        try{
+            throw new Error('Erro de sintaxe')
+        }catch(erro){
+            // erro
+            console.log('ALGO DEU ERRADO!!!')
+            return
+        }
+    }
+
+    console.log('CHEGOU AQUI...')
+
+    if(codigo < 1 || codigo > 10 && maximoDeAlunos < 5 || maximoDeAlunos > 10){
+        throw new Error('O codigo ou o maximo de alunos esta fora do escopo planejado')
+    }
+
+    if(!turmaEncontrada){
+        turmas.push({ codigo, maximoDeAlunos})
+    }
+
+    return turmas
 }
 
-console.log(aluno, turma)
+// console.log(turma)
+console.log('SAÍDA', turmas)
+
+
+
+    // const turma = 
+    //     {
+    //         codigo: 10, 
+    //         maximo: 10
+    //     }
+
+
+    // const turmas = []
+
+    // const cadastrarTurma = ({codigo, maximo}) => { // transformar em objeto para saber os parâmetros no console 
+
+    //    console.log(turmas.find(turma => turma.codigo === codigo))
+    //    turmas.push({ codigo, maximo}) //adiciona valor no final
+    //    return turmas
+    // }
+
+    // cadastrarTurma({codigo: 1, maximo: 10}) //objeto
+    // cadastrarTurma({codigo: 1, maximo: 10}) // sem 'código' repetido
+
+    // console.log(turmas)
+
+// console.log(turma)
