6a3de8a5e9d8bf472f721ce7e37d2b41177b8692
Camila Feitosa <114369214+camfeitosa@users.noreply.github.com>
2023-08-12 16:14:38 -0300

    edit

--- lp/projetoFinal/script.js
+++ lp/projetoFinal/script.js
@@ -9,9 +9,6 @@
 //Cadastrar turma
 // Turma com o código existente = duplicado
 
-
-// EXEMPLO DO PROF
-
 const turma = {
     codigo: 0,
     maximoDeAlunos: 0 
@@ -54,8 +51,7 @@ const cadastrarTurma = ({codigo, maximoDeAlunos}) => {
 
 // console.log(turma)
 console.log('SAÍDA', turmas)
-
-
+cadastrarTurma({codigo: 8, maximo: 10}) //objeto
 
     // const turma = 
     //     {
@@ -79,3 +75,37 @@ console.log('SAÍDA', turmas)
     // console.log(turmas)
 
 // console.log(turma)
+
+
+
+//TESTARRRRRRRRRRRRRRRRRRRRRRRRRRRRR
+// const turmas = [];
+
+// // Cadastro de turmas
+// // Verificar se a turma com o código já existe
+// const cadastrarTurma = ({ codigo, maximoDeAlunos }) => {
+//     codigo = parseInt(codigo);
+//     maximoDeAlunos = parseInt(maximoDeAlunos);
+
+//     if (isNaN(codigo) || isNaN(maximoDeAlunos)) {
+//         console.log('ALGO DEU ERRADO!!!');
+//         return;
+//     }
+
+//     if (codigo < 1 || codigo > 10 || maximoDeAlunos < 5 || maximoDeAlunos > 10) {
+//         throw new Error('O código ou o máximo de alunos está fora do escopo planejado');
+//     }
+
+//     const turmaEncontrada = turmas.find(turma => turma.codigo === codigo);
+
+//     if (!turmaEncontrada) {
+//         turmas.push({ codigo, maximoDeAlunos });
+//     }
+
+//     console.log('CHEGOU AQUI...');
+//     return turmas;
+// }
+
+// console.log('ANTES', turmas);
+// cadastrarTurma({ codigo: 8, maximoDeAlunos: 10 });
+// console.log('SAÍDA', turmas);
